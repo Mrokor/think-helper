8c3d5eeeaa7968e87bde8cb8ee5d32b09a4ef0b7
OkCoder <1046512080@qq.com>
2022-06-15 10:11:23 +0800

    no message

---
+++ src/helper.php
@@ -0,0 +1,14 @@
+<?php
+
+use think\Response;
+
+if (!function_exists('apiReturn')) {
+    function apiReturn($data = [], string $message = 'SUCCESS', int $code = 0, $statusCode = 200, $header = [], $options = []): Response
+    {
+        return Response::create([
+            'data'    => $data,
+            'code'    => $code,
+            'message' => $message
+        ], 'json', $statusCode)->header($header)->options($options);
+    }
+}
