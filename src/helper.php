0cfbcc1979301f000066cb0cc6bd2297d033d2b0
okcoder <1046512080@qq.com>
2023-01-17 20:00:19 +0800

    no message

--- src/helper.php
+++ src/helper.php
@@ -3,7 +3,7 @@
 use think\response\Json;
 
 if (!function_exists('api_return')) {
-    function apiReturn($data = [], string $message = 'SUCCESS', int $code = 0, $statusCode = 200, $header = [], $options = []): Json
+    function apiReturn($data = [], string $message = '', int $code = 0, $statusCode = 200, $header = [], $options = []): Json
     {
         return json([
             'data' => $data, 'message' => $message, 'code' => $code, 'timestamp' => time()
